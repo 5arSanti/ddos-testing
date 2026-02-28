28b1da27c0d7b5c118129ec1f941c61bf334a257
Santiago Arias <santiari05@hotmail.com>
2026-02-28 17:59:44 -0500

    Add logging to health check endpoint in AppController

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger } from '@nestjs/common';
 
 @Controller()
 export class AppController {
+  private readonly logger = new Logger(AppController.name);
+
   @Get('')
   health() {
+    this.logger.log('Health check requested');
+
     return {
       status: 'up',
       service: 'ddos-testing-api',
