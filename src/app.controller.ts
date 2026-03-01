b8941164ecdcc37a3b553f469c6133ec29be456f
Santiago Arias <santiari05@hotmail.com>
2026-02-28 19:23:20 -0500

    Enhance health check logging to include request count in AppController

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,9 +4,12 @@ import { Controller, Get, Logger } from '@nestjs/common';
 export class AppController {
   private readonly logger = new Logger(AppController.name);
 
+  public counter = 0;
+
   @Get('')
   health() {
-    this.logger.log('Health check requested');
+    this.counter++;
+    this.logger.log(`Health check requested ${this.counter} times`);
 
     return {
       status: 'up',
